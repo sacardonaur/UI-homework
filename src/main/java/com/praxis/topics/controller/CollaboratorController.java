4cab353c57f7532041f2b6c48512025a2b510b55
nbuitragob <nicobuitra96@gmail.com>
2018-03-11 14:14:48 -0500

    Update CollaboratorController.java

--- src/main/java/com/praxis/topics/controller/CollaboratorController.java
+++ src/main/java/com/praxis/topics/controller/CollaboratorController.java
@@ -64,10 +64,6 @@ public class CollaboratorController {
             topicService.addTopic(detail.getTopic());
         }
 
-        else{
-            topicService.updateTopic(detail.getTopic());
-        }
-
         Collaborator collaborator = collaboratorService.getCollaboratorById(id);
         List <Detail> topicsToTeach = collaborator.getTopicsToTeach();
 
